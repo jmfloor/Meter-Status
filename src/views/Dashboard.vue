e2b74fe38aef91b99d37606bd87c55f07e97236f
jmfloor <joe@flowcraft.cloud>
2022-11-15 16:50:46 +0200

    layout changes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,129 +1,6 @@
 <template>
   <div class="py-4 container-fluid">
-    <div class="row">
-      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-        <card
-          :title="stats.money.title"
-          :value="stats.money.value"
-          :percentage="stats.money.percentage"
-          :icon-class="stats.money.iconClass"
-          :icon-background="stats.iconBackground"
-          direction-reverse
-        ></card>
-      </div>
-      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-        <card
-          :title="stats.users.title"
-          :value="stats.users.value"
-          :percentage="stats.users.percentage"
-          :icon-class="stats.users.iconClass"
-          :icon-background="stats.iconBackground"
-          direction-reverse
-        ></card>
-      </div>
-      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-        <card
-          :title="stats.clients.title"
-          :value="stats.clients.value"
-          :percentage="stats.clients.percentage"
-          :icon-class="stats.clients.iconClass"
-          :icon-background="stats.iconBackground"
-          :percentage-color="stats.clients.percentageColor"
-          direction-reverse
-        ></card>
-      </div>
-      <div class="col-xl-3 col-sm-6 mb-xl-0">
-        <card
-          :title="stats.sales.title"
-          :value="stats.sales.value"
-          :percentage="stats.sales.percentage"
-          :icon-class="stats.sales.iconClass"
-          :icon-background="stats.iconBackground"
-          direction-reverse
-        ></card>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-7 mb-lg-0 mb-4">
-        <div class="card">
-          <div class="card-body p-3">
-            <div class="row">
-              <div class="col-lg-6">
-                <div class="d-flex flex-column h-100">
-                  <p class="mb-1 pt-2 text-bold">Built by developers</p>
-                  <h5 class="font-weight-bolder">Vite Soft UI Dashboard</h5>
-                  <p class="mb-5">
-                    From colors, cards, typography to complex elements, you will
-                    find the full documentation.
-                  </p>
-                  <a
-                    class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-auto"
-                    href="javascript:;"
-                  >
-                    Read More
-                    <i
-                      class="fas fa-arrow-right text-sm ms-1"
-                      aria-hidden="true"
-                    ></i>
-                  </a>
-                </div>
-              </div>
-              <div class="col-lg-5 ms-auto text-center mt-5 mt-lg-0">
-                <div class="bg-gradient-success border-radius-lg h-100">
-                  <img
-                    src="../assets/img/shapes/waves-white.svg"
-                    class="position-absolute h-100 w-50 top-0 d-lg-block d-none"
-                    alt="waves"
-                  />
-                  <div
-                    class="position-relative d-flex align-items-center justify-content-center h-100"
-                  >
-                    <img
-                      class="w-100 position-relative z-index-2 pt-4"
-                      src="../assets/img/illustrations/rocket-white.png"
-                      alt="rocket"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-5">
-        <div class="card h-100 p-3">
-          <div
-            class="overflow-hidden position-relative border-radius-lg bg-cover h-100"
-            style="
-              background-image: url('https://demos.creative-tim.com/soft-ui-dashboard/assets/img/ivancik.jpg');
-            "
-          >
-            <span class="mask bg-gradient-dark"></span>
-            <div
-              class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3"
-            >
-              <h5 class="text-white font-weight-bolder mb-4 pt-2">
-                Work with the rockets
-              </h5>
-              <p class="text-white">
-                Wealth creation is an evolutionarily recent positive-sum game.
-                It is all about who take the opportunity first.
-              </p>
-              <a
-                class="text-white text-sm font-weight-bold mb-0 icon-move-right mt-auto"
-                href="javascript:;"
-              >
-                Read More
-                <i
-                  class="fas fa-arrow-right text-sm ms-1"
-                  aria-hidden="true"
-                ></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+
     <div class="mt-4 row">
       <div class="mb-4 col-lg-5 mb-lg-0">
         <div class="card z-index-2">
@@ -140,16 +17,10 @@
         </div>
       </div>
     </div>
-    <div class="row my-4">
-      <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
-        <projects-card />
-      </div>
-      <div class="col-lg-4 col-md-6">
-        <Orders-card />
-      </div>
-    </div>
+
   </div>
 </template>
+
 <script>
 import Card from "@/examples/Cards/Card.vue";
 import ActiveUsersChart from "@/examples/Charts/ActiveUsersChart.vue";
