d6e071614632bbb7af26c9dc205d77d13d710029
jmfloor <joe@flowcraft.cloud>
2022-11-18 00:27:04 +0200

    center cards in row

--- src/views/Meter.vue
+++ src/views/Meter.vue
@@ -14,7 +14,7 @@
       </div>
       
     </div>
-    <div class="row">
+    <div class="row justify-content-center">
       <div class="col-12 col-sm-6 col-md-4 col-xl-3">
         <card
           title="this month"
